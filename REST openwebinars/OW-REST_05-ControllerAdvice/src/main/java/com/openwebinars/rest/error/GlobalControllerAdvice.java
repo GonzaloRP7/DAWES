fbc1526dadb8bffb0a8b29c02be01be4936ed09c
Gonzalo <gonzalorp89@educastur.es>
2020-04-09 12:28:32 +0200

    Añadido curso OW REST 3

---
+++ REST openwebinars/OW-REST_05-ControllerAdvice/src/main/java/com/openwebinars/rest/error/GlobalControllerAdvice.java
@@ -0,0 +1,38 @@
+package com.openwebinars.rest.error;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+	@ExceptionHandler(ProductoNotFoundException.class)
+	public ResponseEntity<ApiError> handleProductoNoEncontrado(ProductoNotFoundException ex) {
+//		ApiError apiError = new ApiError();
+//		apiError.setEstado(HttpStatus.NOT_FOUND);
+//		apiError.setFecha(LocalDateTime.now());
+//		apiError.setMensaje(ex.getMessage());
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+		
+		ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+	}
+	
+	@ExceptionHandler(JsonMappingException.class)
+	public ResponseEntity<ApiError> handleJsonMappingException(JsonMappingException ex) {
+//		ApiError apiError = new ApiError();
+//		apiError.setEstado(HttpStatus.BAD_REQUEST);
+//		apiError.setFecha(LocalDateTime.now());
+//		apiError.setMensaje(ex.getMessage());
+//		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+		
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+	}
+}
